9d58222c0168a5c261c231f92af038da22467d84
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2024-10-27 15:20:46 +0100

    Estimator_Tool_app.py

--- Estimator_Tool_app.py
+++ Estimator_Tool_app.py
@@ -10,11 +10,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import undetected_chromedriver as UC
+#import undetected_chromedriver as UC
 #from playwright.sync_api import sync_playwright
 import pandas as pd
 from io import StringIO
-from pyngrok import ngrok
 import joblib
 import gdown
 import time
